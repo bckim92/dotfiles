b0eea68d7be99f1c4631fd017c6344609433d000
Byeongchang Kim <byeongchang.kim@gmail.com>
2024-11-03 10:46:28 +0900

    vim(copilot): Bind <C-l> to accept suggestion
    Accept copilot's suggestion with <C-l> rather than <tab> (default behavior) in insertion mode. This may conflict with suggested behavior with <C-l> key binding in copilot plugin, which is used for word-level accept, but we can take some time to decide which one is better.

--- nvim/lua/plugins/ide.lua
+++ nvim/lua/plugins/ide.lua
@@ -63,7 +63,12 @@ return {
     },
     config = require('config.completion').setup_cmp,
   };
-  Plug 'github/copilot.vim';
+  Plug 'github/copilot.vim' {
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept('<CR>')", { silent = true, expr = true })
+    end,
+  };
 
   -- Formatting
   Plug 'stevearc/conform.nvim' {
