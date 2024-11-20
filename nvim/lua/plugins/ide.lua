8b8f15890f95cc8529c15b2403aadf2ec1d9d018
Byeongchang Kim <byeongchang.kim@gmail.com>
2024-11-20 14:58:20 +0900

    vim(avante): Add plugin 'avante.nvim'

--- nvim/lua/plugins/ide.lua
+++ nvim/lua/plugins/ide.lua
@@ -63,6 +63,49 @@ return {
     },
     config = require('config.completion').setup_cmp,
   };
+
+  -- LLM based completion
+  Plug 'yetone/avante.nvim' {
+    event = 'VeryLazy',
+    lazy = false,
+    opts = {
+    },
+    build = 'make',
+    dependencies = {
+      --"nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  };
   Plug 'github/copilot.vim' {
     config = function()
       vim.g.copilot_no_tab_map = true
