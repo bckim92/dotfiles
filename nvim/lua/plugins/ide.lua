3a987065a429fe8e4358fb21ffa8a178bf8cbf2a
Byeongchang Kim <byeongchang.kim@gmail.com>
2026-01-11 11:43:58 +0900

    vim: Remove plugin copilot.vim

--- nvim/lua/plugins/ide.lua
+++ nvim/lua/plugins/ide.lua
@@ -109,12 +109,6 @@ return {
       },
     },
   };
-  Plug 'github/copilot.vim' {
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept('<CR>')", { silent = true, expr = true })
-    end,
-  };
 
   -- Formatting
   Plug 'stevearc/conform.nvim' {
